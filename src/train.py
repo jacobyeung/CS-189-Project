7232ee2b7c40f9a28a514665e34f06d89dbaa7f7
Jacob Yeung <jacobyeung@berkeley.edu>
2020-10-28 14:59:52 -0700

    trying pretrained model

--- src/train.py
+++ src/train.py
@@ -119,17 +119,19 @@ for fpath in fpaths:
     print(torch.cuda.is_available())
 
     model = CNN().to(device)
+    model = torch.hub.load('pytorch/vision:v0.6.0',
+                           'vgg11', pretrained=True).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
 
     t_losses = torch.tensor([0] * epochs)
     v_losses = torch.tensor([0] * epochs)
     for epoch in range(epochs):
-        train_loss = train(model, train_loader, epoch, len_train_set)
+        # train_loss = train(model, train_loader, epoch, len_train_set)
         val_loss = validate(model, val_loader, epoch, fpath, len_val_set)
-        t_losses[epoch] = train_loss
+        # t_losses[epoch] = train_loss
         v_losses[epoch] = val_loss
-        print("Train Loss: " + str(train_loss))
+        # print("Train Loss: " + str(train_loss))
         print("Val Loss: " + str(val_loss))
-    np.savez(fpath + "_loss", train_loss=train_loss, val_loss=val_loss)
+    # np.savez(fpath + "_loss", train_loss=train_loss, val_loss=val_loss)
     torch.save(model.state_dict(), "./model_version" + fpath + ".pt")
     visualize(fpath, epochs, t_losses.numpy(), v_losses.numpy())
